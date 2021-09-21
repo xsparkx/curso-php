2379d60f1d8904cca45b7e57bf6e72c4ebbab5d1
xsparkx <81881502+xsparkx@users.noreply.github.com>
2021-09-21 19:41:02 -0300

    final do terceiro módulo curso de PHP Cod3r

---
+++ exercicio.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Oswald:wght@200;300;400;500;600;700&display=swap" rel="stylesheet">    
+    <title>Curso PHP</title>
+</head>
+<body>
+        <header class="cabecalho">
+            <h1>Curso PHP</h1>
+            <h2>Visualização do exercício</h2>
+        </header>
+        <nav class="navegacao">
+            <ul>
+                <li class="verde"><a href="">Sem frmatação</a></li>
+                <li class="vermelho"><a href="">Voltar</a></li>
+            </ul>
+        </nav>
+        <main class="principal">
+            <div class="conteudo">  
+            </div>
+        </main>
+        <footer class="rodape">
+            COD3R 2021 - Designed by <a href="#"> Douglas Rodrigues</a>
+        </footer>
+    
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,80 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="stylesheet" href="assets/css/exercicio.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Oswald:wght@200;300;400;500;600;700&display=swap" rel="stylesheet">    
+    <title>Curso PHP</title>
+</head>
+<body class="exercicio">
+        <header class="cabecalho">
+            <h1>Curso PHP</h1>
+            <h2>Índices do Curso</h2>
+        </header>
+            <nav class="navegacao">
+                <a href="" class="verde">Sem formatação</a>
+                <a href="" class="verde">Voltar</a>
+            </nav>
+        <main class="principal">
+            <div class="conteudo">
+                <nav class="modulos">
+                   <div class="modulo verde">
+                       <h3>Módulo 01</h3>
+                       <ul>
+                           <li><a href="exercicio.php">Exercício A</a></li>
+                           <li><a href="#">Exercício B</a></li>
+                           <li><a href="#">Exercício B</a></li>
+                        </ul>
+                   </div>
+                   <div class="modulo azul">
+                    <h3>Módulo 02</h3>
+                    <ul>
+                        <li><a href="#">Exercício A</a></li>
+                        <li><a href="#">Exercício B</a></li>
+                        <li><a href="#">Exercício B</a></li>
+                     </ul>
+                </div>
+                <div class="modulo roxo">
+                    <h3>Módulo 03</h3>
+                    <ul>
+                        <li><a href="#">Exercício A</a></li>
+                        <li><a href="#">Exercício B</a></li>
+                        <li><a href="#">Exercício B</a></li>
+                     </ul>
+                </div>
+                <div class="modulo vermelho">
+                    <h3>Módulo 04</h3>
+                    <ul>
+                        <li><a href="#">Exercício A</a></li>
+                        <li><a href="#">Exercício B</a></li>
+                        <li><a href="#">Exercício B</a></li>
+                     </ul>
+                </div>
+                <div class="modulo laranja">
+                    <h3>Módulo 05</h3>
+                    <ul>
+                        <li><a href="#">Exercício A</a></li>
+                        <li><a href="#">Exercício B</a></li>
+                        <li><a href="#">Exercício B</a></li>
+                     </ul>
+                </div>
+                <div class="modulo pink">
+                    <h3>Módulo 06</h3>
+                    <ul>
+                        <li><a href="#">Exercício A</a></li>
+                        <li><a href="#">Exercício B</a></li>
+                        <li><a href="#">Exercício B</a></li>
+                     </ul>
+                </div>
+                </nav>
+            </div>
+        </main>
+        <footer class="rodape">
+            COD3R 2021 - Designed by <a href="#"> Douglas Rodrigues</a>
+        </footer>
+    
+</body>
+</html>
